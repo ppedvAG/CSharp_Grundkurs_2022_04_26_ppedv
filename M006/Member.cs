bd39f5f03c7ddf584c159f2063cc5919e09ace4a
ppedvLukasK <lukask@ppedv.de>
2022-04-26 16:26:04 +0200

    M006

---
+++ M006/Member.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M006
+{
+	public class Member
+	{
+		private string name;
+
+		public string Name //Property: private Variable im Hintergrund
+		{
+			get => name;
+			set
+			{
+				if (!string.IsNullOrEmpty(value) && value.Length >= 2)
+					name = value;
+			}
+		}
+
+
+		public string Vorname { get; private set; } = "Max";
+
+
+		private int gehalt;
+
+		public int Gehalt => gehalt; //Get-only Property
+
+
+		public string Lieblingsfarbe { get; init; } = "Blau"; //init: Nur hier oder im Konstruktor setzbar
+
+		//ctor + Tab + Tab
+		public Member()
+		{
+			Name = "Max";
+			Vorname = "Max";
+			gehalt = 2000;
+			Lieblingsfarbe = "Rot";
+		}
+
+		public Member(string name, string vorname, int gehalt, string farbe)
+		{
+			Name = name;
+			Vorname = vorname;
+			this.gehalt = gehalt;
+			Lieblingsfarbe = farbe;
+		}
+
+		public void Test() => Console.WriteLine("Ich bin ein Test");
+	}
+}
---
+++ M006/Program.cs
@@ -0,0 +1,40 @@
+﻿using M006.UnternamespaceAusserhalb;
+using M006.UnternamespaceInnerhalb;
+using System;
+using System.IO;
+using System.Linq;
+using static System.Environment; //gibt statische Methoden aus der Klasse weiter
+
+namespace M006
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			UnterklasseAusserhalb ua;
+			UnterklasseInnerhalb uia;
+
+			Console.WriteLine(); //System
+			Path.GetFullPath(""); //System.IO
+			new[] { 1, 2, 3 }.Sum(); //System.Linq
+			Exit(0); //aus Environment Klasse
+
+			Member m = new Member(); //Objektinitialisierung mit new (Konstruktor ohne Parameter aufrufen)
+			m.Name = "Max"; //set-Property wie Variable verwenden
+			Console.WriteLine(m.Name); //get-Property wie Variable verwenden
+			m.Test(); //Methodenaufruf
+			//m.Vorname = "Max"; //nicht möglich da private set
+			//m.Lieblingsfarbe = "Grün"; //nicht möglich da init
+		}
+	}
+
+	namespace UnternamespaceInnerhalb
+	{
+		public class UnterklasseInnerhalb { }
+	}
+}
+
+namespace M006.UnternamespaceAusserhalb
+{
+	public class UnterklasseAusserhalb { }
+}

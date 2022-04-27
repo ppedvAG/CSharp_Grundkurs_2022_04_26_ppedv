8ff1ec0baadeb682491f3f800b0c34d7184969b9
ppedvLukasK <lukask@ppedv.de>
2022-04-27 10:27:36 +0200

    M008

---
+++ M008/AccessModifier.cs
@@ -0,0 +1,20 @@
+﻿namespace M008
+{
+	internal class AccessModifier { }
+
+	class Mensch //Klasse ohne Modifier ist Internal
+	{
+		public string Name { get; set; } //Kann überall gesehen werden (auch außerhalb vom Projekt)
+
+		private int Groesse { get; set; } //Kann nur in Mensch gesehen werden
+
+		internal string Wohnort { get; set; } //Kann überall gesehen werden aber nur im Projekt
+
+
+		protected string Lieblingsfarbe { get; set; } //Kann nur in Mensch und Unterklassen gesehen werden (auch außerhalb vom Projekt)
+
+		protected internal string Lieblingsnahrung { get; set; } //Kann im gesamten Projekt gesehen werden und in Unterklassen außerhalb
+
+		protected private DateTime Geburtsdatum { get; set; } //Kann nur in Mensch und Unterklassen von Mensch gesehen werden (nur im Projekt)
+	}
+}
---
+++ M008/Program.cs
@@ -0,0 +1,14 @@
+﻿using static Vererbung;
+
+public class Program
+{
+	static void Main(string[] args)
+	{
+		Mensch m = new Mensch("Max", 23);
+		m.Name = "Test"; //Name wird übernommen
+		m.WasBinIch();
+
+		Lebewesen lw = new Mensch("Max", 23);
+		lw.PrintName();
+	}
+}

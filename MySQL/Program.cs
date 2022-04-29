aefacecf127ff74ad331e349dc49abe3e2f29ff2
ppedvLukasK <lukask@ppedv.de>
2022-04-29 14:03:03 +0200

    SQL

---
+++ MySQL/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+
+namespace MySQL
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			SQLDriver driver = new SQLDriver("server=localhost;port=3306;user=root;password=root;database=mysql");
+			driver.OpenConnection();
+
+			driver.ExecuteWithoutResult("CREATE TABLE test(id int, vorname varchar(20));");
+			driver.ExecuteWithoutResult("INSERT INTO test VALUES (0, 'Max')");
+			driver.ExecuteWithoutResult("INSERT INTO test VALUES (1, 'Peter')");
+			driver.ExecuteWithoutResult("INSERT INTO test VALUES (2, 'Hans')");
+			DataTable dt = driver.ExecuteWithResult("SELECT * FROM test");
+
+			List<Person> p = new List<Person>();
+			for (int i = 0; i < dt.Rows.Count; i++)
+			{
+				p.Add(new Person((int) dt.Rows[i].ItemArray[0], (string) dt.Rows[i].ItemArray[1]));
+			}
+		}
+	}
+
+	public record Person(int ID, string Vorname);
+}
---
+++ MySQL/SQLDriver.cs
@@ -0,0 +1,110 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+public class SQLDriver
+{
+	private MySqlConnection CurrentConnection { get; set; }
+
+	public bool IsConnected => CurrentConnection != null && CurrentConnection.State != ConnectionState.Closed && CurrentConnection.State != ConnectionState.Connecting;
+
+	//server=localhost;port=3306;user=root;password=root;database=mysql
+	public SQLDriver(string connectionString) => CurrentConnection = new MySqlConnection(connectionString);
+
+	~SQLDriver() => CurrentConnection?.Dispose();
+
+	//https://dev.mysql.com/doc/mysql-errors/8.0/en/server-error-reference.html
+	public int OpenConnection()
+	{
+		if (IsConnected)
+			return -1;
+
+		try
+		{
+			CurrentConnection.Close(); //Closen wenn Verbindung out-of-sync (zwischen Server und Client)
+			CurrentConnection.Open();
+			return 0;
+		}
+		catch (MySqlException ex)
+		{
+			int errorCode = ex.Number == 0 ? (ex.InnerException as MySqlException).Number : ex.Number;
+			switch (errorCode)
+			{
+				case 1042:
+					Console.WriteLine("Verbindung kann nicht hergestellt werden"); //IP-Adresse oder Port falsch
+					break;
+				case 1045:
+					Console.WriteLine("Falscher User/Passwort");
+					break;
+				case 1049:
+					Console.WriteLine("Invalider Datenbankname");
+					break;
+				default:
+					Console.WriteLine($"Anderer Code {errorCode}: https://dev.mysql.com/doc/mysql-errors/8.0/en/server-error-reference.html");
+					break;
+			}
+			return errorCode;
+		}
+	}
+
+	public void CloseConnection()
+	{
+		CurrentConnection.Close();
+	}
+
+	public bool TestConnection()
+	{
+		if (CurrentConnection?.Ping() == false)
+		{
+			CloseConnection();
+			return false;
+		}
+		return true;
+	}
+
+	public int Reconnect() => TestConnection() ? 0 : OpenConnection();
+
+	public int ExecuteWithoutResult(string sql)
+	{
+		if (Reconnect() != 0)
+		{
+			return -1; //Keine Verbindung möglich
+		}
+
+		using (MySqlCommand cmd = CurrentConnection.CreateCommand())
+		{
+			try
+			{
+				cmd.CommandText = sql;
+				return cmd.ExecuteNonQuery();
+			}
+			catch (MySqlException ex) //Syntax Error meistens
+			{
+				return -1;
+			}
+		}
+	}
+
+	public DataTable ExecuteWithResult(string sql)
+	{
+		if (Reconnect() != 0)
+		{
+			return null; //Keine Verbindung möglich
+		}
+
+		using (MySqlCommand command = CurrentConnection.CreateCommand())
+		{
+			try
+			{
+				command.CommandText = sql;
+				MySqlDataReader reader = command.ExecuteReader();
+				DataTable dt = new DataTable();
+				dt.Load(reader);
+				return dt;
+			}
+			catch (MySqlException ex)
+			{
+				return null;
+			}
+		}
+	}
+}
